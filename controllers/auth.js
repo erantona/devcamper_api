512e86f8153425adc9a12ea82b953b7f46fc36e3
erantona <roysuvendu1557@gmail.com>
2020-12-11 15:09:38 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,7 @@ const asyncHandler = require('../middleware/async');
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
+  //   Create user
   const user = await User.create({
     name,
     email,
